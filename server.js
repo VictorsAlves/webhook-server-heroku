dd90b31019e71bf07c172d7e9354c953b32f21aa
victor-de-s-alves_accent <victor.de.s.alves@accenture.com>
2025-11-03 17:42:43 -0300

    removendo ui

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ const PORT = process.env.PORT || 3000;
 // Permite que o Express leia JSON no corpo da requisição
 app.use(bodyParser.json());
 
-// Serve arquivos estáticos (pasta ui)
-app.use(express.static(path.join(__dirname, "ui")));
-
-// Página principal (dashboard visual)
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "ui", "index.html"));
-});
 // Importa e configura as rotas do clean-conversation
 require("./service/remove-conversations")(app);
 require("./service/fetch-conversations")(app);
